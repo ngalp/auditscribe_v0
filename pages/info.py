4895dc0907f83f183efceb2acf9ea942e205a816
ngalp <ng.alphania@gmail.com>
2024-10-08 06:55:43 +0000

    add data scatter chart

--- pages/info.py
+++ pages/info.py
@@ -3,20 +3,16 @@ import pandas as pd
 import numpy as np
 
 
-st.title('Uber pickups in NYC')
+st.title('Transport Claim Irregularities')
 
   
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATA_URL = ('https://github.com/ngalp/auditscribe/blob/main/data/data.csv')
 
 @st.cache_data
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
 data_load_state = st.text('Loading data...')
@@ -27,13 +23,14 @@ if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+st.subheader('Value of Claims by Number of Trips')
+#hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+#st.bar_chart(hist_values)
+st.scatter_chart(data=data, x="Count", y="Value", x_label="Count", y_label="Value")
 
 # Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+#hour_to_filter = st.slider('hour', 0, 23, 17)
+#filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+#st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+#st.map(filtered_data)
