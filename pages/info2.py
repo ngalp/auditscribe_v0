31b143da7b0c0e2d57dbed5996c4799f13f0fadf
ngalp <ng.alphania@gmail.com>
2024-10-09 02:28:52 +0000

    test sidebar

--- pages/info2.py
+++ pages/info2.py
@@ -1,34 +1,39 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+from openai import OpenAI
 
-
-st.title('Data Analysis')
-
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="file_qa_api_key", type="password")
   
-DATA_URL = ('https://raw.githubusercontent.com/ngalp/auditscribe/refs/heads/main/data/data.csv')
+st.title("📝 Document Q&A with OpenAI")
+uploaded_file = st.file_uploader("Upload an article", type=("txt", "md"))
+question = st.text_input(
+    "Ask something about the article",
+    placeholder="Can you give me a short summary?",
+    disabled=not uploaded_file,
+)
 
-@st.cache_data
-def load_data(url):
-    data = pd.read_csv(url)
-    return data
+if uploaded_file and question and not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.")
 
-data_load_state = st.text('Loading data...')
-data = load_data(DATA_URL)
-data_load_state.text("Done! (using st.cache_data)")
+if uploaded_file and question and openai_api_key:
+    # Create an OpenAI client.
+    client = OpenAI(api_key=openai_api_key)
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
+    # Process the uploaded file and question.
+    document = uploaded_file.read().decode()
+    messages = [
+        {
+            "role": "user",
+            "content": f"Here's a document: {document} \n\n---\n\n {question}",
+        }
+        ]
 
-st.subheader('Value of Claims by Number of Trips')
-#hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-#st.bar_chart(hist_values)
-st.scatter_chart(data=data, x="Count", y="Value", x_label="Count", y_label="Value")
+    # Generate an answer using the OpenAI API.
+    stream = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        stream=True,
+        )
 
-# Some number in the range 0-23
-#hour_to_filter = st.slider('hour', 0, 23, 17)
-#filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-#st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-#st.map(filtered_data)
+    # Stream the response to the app using `st.write_stream`.
+    st.write_stream(stream)
