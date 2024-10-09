f11479643fa1e3ddb62e75c5b1f28f89b1dc27db
ngalp <ng.alphania@gmail.com>
2024-10-09 02:17:50 +0000

    test openai

--- pages/info.py
+++ pages/info.py
@@ -1,12 +1,10 @@
 import streamlit as st
-import anthropic
+from openai import OpenAI
 
 with st.sidebar:
-    anthropic_api_key = st.text_input("Anthropic API Key", key="file_qa_api_key", type="password")
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/1_File_Q%26A.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
-
-st.title("📝 File Q&A with Anthropic")
+    openai_api_key = st.text_input("OpenAI API Key", key="file_qa_api_key", type="password")
+  
+st.title("📝 Document Q&A with OpenAI")
 uploaded_file = st.file_uploader("Upload an article", type=("txt", "md"))
 question = st.text_input(
     "Ask something about the article",
@@ -14,20 +12,28 @@ question = st.text_input(
     disabled=not uploaded_file,
 )
 
-if uploaded_file and question and not anthropic_api_key:
-    st.info("Please add your Anthropic API key to continue.")
+if uploaded_file and question and not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.")
 
-if uploaded_file and question and anthropic_api_key:
-    article = uploaded_file.read().decode()
-    prompt = f"""{anthropic.HUMAN_PROMPT} Here's an article:\n\n<article>
-    {article}\n\n</article>\n\n{question}{anthropic.AI_PROMPT}"""
+if uploaded_file and question and openai_api_key:
+    # Create an OpenAI client.
+    client = OpenAI(api_key=openai_api_key)
 
-    client = anthropic.Client(api_key=anthropic_api_key)
-    response = client.completions.create(
-        prompt=prompt,
-        stop_sequences=[anthropic.HUMAN_PROMPT],
-        model="claude-v1",  # "claude-2" for Claude 2 model
-        max_tokens_to_sample=100,
-    )
-    st.write("### Answer")
-    st.write(response.completion)
+    # Process the uploaded file and question.
+    document = uploaded_file.read().decode()
+    messages = [
+        {
+            "role": "user",
+            "content": f"Here's a document: {document} \n\n---\n\n {question}",
+        }
+        ]
+
+    # Generate an answer using the OpenAI API.
+    stream = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        stream=True,
+        )
+
+    # Stream the response to the app using `st.write_stream`.
+    st.write_stream(stream)
